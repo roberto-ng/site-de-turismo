b117cab9ef32eea8941088c4ae7c49cc89ee4d7f
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2022-03-08 17:33:31 -0300

    Mostrar tela de resultados

--- components/FormPassagens.tsx
+++ components/FormPassagens.tsx
@@ -16,57 +16,78 @@ export default function FormPassagens() {
     const [hoje, setHoje] = useState(new Date());
     const [amanha, setAmanha] = useState(calcularAmanha());
 
-    return (
+    const handleBuscarClick = () => {
+        setMostrarResultado(true);
+    };
+
+    const handleVoltarClick = () => {
+        setMostrarResultado(false);
+    }
+
+    if (mostrarResultado) {
+        return (
             <div id={styles.caixaPesquisa}>
-                <div className={styles.linha}>
-                    <p className={styles.titulo}>
-                        Passagens aérias
-                    </p>
-                </div>
-
-                <div className={styles.linha}>
-                    <div className={styles.item}>
-                        <label>Origem: </label>
-                        <input 
-                            type="text" 
-                            className={styles.textInput}
-                            placeholder="Rio de Janeiro"
-                        />
-                    </div>
-
-                    <div className={styles.item}>
-                        <label>Destino: </label>
-                        <input 
-                            type="text" 
-                            className={styles.textInput}
-                            placeholder="Brasília"
-                        />
-                    </div>
-                </div>
-
-                <div className={styles.linha}>
-                    <div className={styles.item}>
-                        <label>Ida: </label>
-                        <input 
-                            type="text" 
-                            className={styles.textInput}
-                            defaultValue={hoje.toLocaleDateString()}
-                        />
-                    </div>
-
-                    <div className={styles.item}>
-                        <label>Volta: </label>
-                        <input 
-                            type="text" 
-                            className={styles.textInput}
-                            defaultValue={amanha.toLocaleDateString()}
-                        />
-                    </div>
-                </div>
-
-                <div className={styles.linha}>
-                    <button>Buscar</button>
-                </div>
+                <p>Nenhum resultado encontrado.</p>
+                <button onClick={handleVoltarClick} className={styles.button}>
+                    Voltar
+                </button>
             </div>
-    );
+        );
+    } else {
+        return (
+                <div id={styles.caixaPesquisa}>
+                    <div className={styles.linha}>
+                        <p className={styles.titulo}>
+                            Passagens aérias
+                        </p>
+                    </div>
+    
+                    <div className={styles.linha}>
+                        <div className={styles.item}>
+                            <label>Origem: </label>
+                            <input 
+                                type="text" 
+                                className={styles.textInput}
+                                placeholder="Rio de Janeiro"
+                            />
+                        </div>
+    
+                        <div className={styles.item}>
+                            <label>Destino: </label>
+                            <input 
+                                type="text" 
+                                className={styles.textInput}
+                                placeholder="Brasília"
+                            />
+                        </div>
+                    </div>
+    
+                    <div className={styles.linha}>
+                        <div className={styles.item}>
+                            <label>Ida: </label>
+                            <input 
+                                type="text" 
+                                className={styles.textInput}
+                                defaultValue={hoje.toLocaleDateString()}
+                            />
+                        </div>
+    
+                        <div className={styles.item}>
+                            <label>Volta: </label>
+                            <input 
+                                type="text" 
+                                className={styles.textInput}
+                                defaultValue={amanha.toLocaleDateString()}
+                            />
+                        </div>
+                    </div>
+    
+                    <div className={styles.linha}>
+                        <button onClick={handleBuscarClick} className={styles.button}>
+                            Buscar
+                        </button>
+                    </div>
+                </div>
+        );
+    }
 }
